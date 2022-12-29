62e48bf22120a2516ff7734d58acf0fac61ad41b
kellylino <kellylino@163.com>
2022-12-29 15:46:25 +0200

    test

--- api/index.py
+++ api/index.py
@@ -1,17 +1,11 @@
-from urllib.request import urlopen
-from xml.etree.ElementTree import parse
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-drone_info = []
-
-
 @app.route('/')
-def webpage():
-    global drone_info
-    drone_url = urlopen('https://assignments.reaktor.com/birdnest/drones')
-    report = parse(drone_url)
-    deviceId = report.find("deviceInformation").get("deviceId")
+def home():
+    return 'Hello, World!'
 
-    return render_template("table.html", deviceId=deviceId)
+@app.route('/about')
+def about():
+    return 'About'
