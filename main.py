cf46d41745836580c7446ef911d97f2c4dff2521
kellylino <kellylino@163.com>
2022-12-29 09:08:54 +0200

    initial version

---
+++ main.py
@@ -0,0 +1,55 @@
+import math
+import json
+from datetime import datetime, timedelta
+from urllib.request import urlopen
+from xml.etree.ElementTree import parse
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+drone_info = []
+
+
+@app.route('/')
+def webpage():
+    global drone_info
+    drone_url = urlopen('https://assignments.reaktor.com/birdnest/drones')
+    report = parse(drone_url)
+
+    for drone in report.iterfind("capture/drone"):
+        x = math.pow(abs(float(drone.findtext("positionX")) - 250000), 2) + math.pow(
+            abs(float(drone.findtext("positionY")) - 250000), 2)
+        distance = math.sqrt(x)
+        if distance <= 100000:
+            mydict = {}
+            now = datetime.now()
+            drone.findtext("serialNumber")
+            mydict["serialNumber"] = drone.findtext("serialNumber")
+            mydict["datetime"] = now.strftime("%d/%m/%Y %H:%M:%S")
+            mydict["distance"] = distance
+            drone_info.append(mydict)
+
+        for i in drone_info:
+            serialNumber_url = urlopen("https://assignments.reaktor.com/birdnest/pilots/" + i["serialNumber"])
+            data_json = json.loads(serialNumber_url.read())
+            i["firstName"] = data_json["firstName"]
+            i["lastName"] = data_json["lastName"]
+            i["email"] = data_json["email"]
+            i["phoneNumber"] = data_json["phoneNumber"]
+
+        closest_distance = 100000.
+        new_drone_info = []
+        for i in drone_info:
+            if datetime.now() - datetime.strptime(i["datetime"], "%d/%m/%Y %H:%M:%S") < timedelta(minutes=10):
+                if i["distance"] < closest_distance:
+                    closest_distance = i["distance"]
+                new_drone_info.append(i)
+
+        drone_info = new_drone_info
+
+        return render_template("table.html", len=len(drone_info), drone_info=drone_info, closest_distance=closest_distance)
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, debug=True)
