0a0c42c0185d9d08e6506abf4ce1d39c9104130d
kellylino <kellylino@163.com>
2023-01-06 22:41:55 +0200

    final version 1.0

--- api/index.py
+++ api/index.py
@@ -1,96 +1,86 @@
 import math
 import json
+import time
 from datetime import datetime, timedelta
 from urllib.request import urlopen
 from xml.etree.ElementTree import parse
 from flask import Flask, render_template, jsonify
+from apscheduler.schedulers.background import BackgroundScheduler
+from threading import Lock
+
 
 app = Flask(__name__)
 
 
-drone_info = []
+pilot_info = []
+closest_distance = 100
+data_lock = Lock()
+
+
+
+def service():
+    print(time.ctime(), "Service")
+
+    global pilot_info, closest_distance
+    with data_lock:
+        drone_url = urlopen('https://assignments.reaktor.com/birdnest/drones')
+        report = parse(drone_url)
+        capture = report.find("capture")
+
+        # find out the violated no drone zone rule pilots
+        for drone in report.iterfind("capture/drone"):
+            x = math.pow(abs(float(drone.findtext("positionX")) - 250000), 2) + math.pow(
+                abs(float(drone.findtext("positionY")) - 250000), 2)
+            distance = math.sqrt(x)
+            if distance <= 100000:
+                distance = int(distance / 1000)
+                if distance < closest_distance:
+                    closest_distance = distance
+                mydict = {}
+                drone.findtext("serialNumber")
+                mydict["serialNumber"] = drone.findtext("serialNumber")
+                mydict["datetime"] = datetime.strptime(capture.get("snapshotTimestamp"), "%Y-%m-%dT%H:%M:%S.%fZ")
+                mydict["distance"] = distance
+                pilot_info.append(mydict)
+
+        # stored the needed relative information of the pilot
+        for i in pilot_info:
+            serialNumber_url = urlopen("https://assignments.reaktor.com/birdnest/pilots/" + i["serialNumber"])
+            data_json = json.loads(serialNumber_url.read())
+            i["firstName"] = data_json["firstName"]
+            i["lastName"] = data_json["lastName"]
+            i["email"] = data_json["email"]
+            i["phoneNumber"] = data_json["phoneNumber"]
+
+        # find out the closest distance
+        if len(pilot_info) > 0:
+            new_pilot_info = []
+            latest_snapshot_time = pilot_info[-1]["datetime"]
+            for i in pilot_info:
+                if latest_snapshot_time - i["datetime"] < timedelta(minutes=10):
+                    new_pilot_info.append(i)
+
+            pilot_info = new_pilot_info
+        return pilot_info, closest_distance
 
 
 @app.route('/')
 def webpage():
-    global drone_info
-    drone_url = urlopen('https://assignments.reaktor.com/birdnest/drones')
-    report = parse(drone_url)
-
-    # find out the violated no drone zone rule pilots
-    for drone in report.iterfind("capture/drone"):
-        x = math.pow(abs(float(drone.findtext("positionX")) - 250000), 2) + math.pow(
-            abs(float(drone.findtext("positionY")) - 250000), 2)
-        distance = math.sqrt(x)
-        if distance <= 100000:
-            mydict = {}
-            now = datetime.now()
-            drone.findtext("serialNumber")
-            mydict["serialNumber"] = drone.findtext("serialNumber")
-            mydict["datetime"] = now.strftime("%d/%m/%Y %H:%M:%S")
-            mydict["distance"] = distance
-            drone_info.append(mydict)
-
-    # stored the needed relative information of the pilot
-    for i in drone_info:
-        serialNumber_url = urlopen("https://assignments.reaktor.com/birdnest/pilots/" + i["serialNumber"])
-        data_json = json.loads(serialNumber_url.read())
-        i["firstName"] = data_json["firstName"]
-        i["lastName"] = data_json["lastName"]
-        i["email"] = data_json["email"]
-        i["phoneNumber"] = data_json["phoneNumber"]
-
-    # find out the closest distance
-    closest_distance = 100
-    new_drone_info = []
-    for i in drone_info:
-        if datetime.now() - datetime.strptime(i["datetime"], "%d/%m/%Y %H:%M:%S") < timedelta(minutes=10):
-            if i["distance"] < closest_distance:
-                closest_distance = i["distance"]
-                new_drone_info.append(i)
-
-    drone_info = new_drone_info
-    return render_template("table.html", len=len(drone_info), drone_info=drone_info, closest_distance=closest_distance)
+    with data_lock:
+        return render_template("table.html", len=len(pilot_info), pilot_info=pilot_info, closest_distance=closest_distance)
 
 
 @app.route('/update', methods=["GET"])
 # update part of the webpage content
 def update():
-    global drone_info
-    drone_url = urlopen('https://assignments.reaktor.com/birdnest/drones')
-    report = parse(drone_url)
+    with data_lock:
+        return jsonify({"pilot_info": pilot_info, "closest_distance": closest_distance})
 
-    for drone in report.iterfind("capture/drone"):
-        x = math.pow(abs(float(drone.findtext("positionX")) - 250000), 2) + math.pow(
-            abs(float(drone.findtext("positionY")) - 250000), 2)
-        distance = math.sqrt(x)
-        if distance <= 100000:
-            distance = int(distance/1000)
-            mydict = {}
-            now = datetime.now()
-            drone.findtext("serialNumber")
-            mydict["serialNumber"] = drone.findtext("serialNumber")
-            mydict["datetime"] = now.strftime("%d/%m/%Y %H:%M:%S")
-            mydict["distance"] = distance
-            drone_info.append(mydict)
 
-    for i in drone_info:
-        serialNumber_url = urlopen("https://assignments.reaktor.com/birdnest/pilots/" + i["serialNumber"])
-        data_json = json.loads(serialNumber_url.read())
-        i["firstName"] = data_json["firstName"]
-        i["lastName"] = data_json["lastName"]
-        i["email"] = data_json["email"]
-        i["phoneNumber"] = data_json["phoneNumber"]
+_scheduler = BackgroundScheduler()
+_scheduler.add_job(service, "interval", seconds=2)
+_scheduler.start()
 
-    closest_distance = 100
-    new_drone_info = []
-    for i in drone_info:
-        if datetime.now() - datetime.strptime(i["datetime"], "%d/%m/%Y %H:%M:%S") < timedelta(minutes=10):
-            if i["distance"] < closest_distance:
-                closest_distance = i["distance"]
-                new_drone_info.append(i)
 
-    drone_info = new_drone_info
-
-    return jsonify({"drone_info": drone_info, "closest_distance": closest_distance})
-
+if __name__ == '__main__':
+    app.run()
